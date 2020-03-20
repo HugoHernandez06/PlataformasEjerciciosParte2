fe976af73c410f316d4d3bd450f8d60089354d6b
HugoHernandez06 <44584564+HugoHernandez06@users.noreply.github.com>
2020-03-20 10:15:33 -0500

    Add files via upload

---
+++ EjerciciosProgramacion/Maraton2/Maraton2/Entrada.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Maraton2
+{
+    public class Entrada
+    {
+        string Nombre;
+        Valvula Val;
+        double Flujo;
+
+        public Entrada()
+        {
+        }
+
+        public Entrada(double flujo)
+        {
+            this.Flujo = flujo;
+        }
+
+        public Entrada(string nombre, Valvula val, double flujo)
+        {
+            this.Nombre = nombre;
+            this.Val = val;
+            this.Flujo = flujo;
+        }
+
+        public string Nombre1 { get => Nombre; set => Nombre = value; }
+        public Valvula Val1 { get => Val; set => Val = value; }
+        public double Flujo1 { get => Flujo; set => Flujo = value; }
+    }
+}
